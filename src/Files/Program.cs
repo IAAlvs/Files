c7a1de4cdba422b808a2c87b20d3f61188e14cb3
IAAlvs <isaacalf.alvs@gmail.com>
2023-07-25 18:51:28 -0600

    feat/Migration Files and Chunks

--- src/Files/Program.cs
+++ src/Files/Program.cs
@@ -33,6 +33,7 @@ try
 catch (Exception ex)
 {
     var type = ex.GetType().Name;
+    if (type.Equals("HostAbortedException", StringComparison.Ordinal)) throw;
     if (type.Equals("StopTheHostException", StringComparison.Ordinal)) throw;
 
     Log.Fatal(ex, "Unhandled exception");
